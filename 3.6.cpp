d5c6a74dce8fe78f18ed0283796dee2905882cdb
Jeswin <jeswincyriac.k@gmail.com>
2018-12-30 12:20:57 +0530

    crack

---
+++ 3.6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main()
+{
+    list int
+<}
+//u can use list and do this pretty easy
+3
---
+++ bitoperations.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=8;
+    cout<<(~x);
+    cout<<(x >> 2);
+    cout<<(x << 2);
+    return 0;
+}
